53f75bbae955ba70ca45047c7205ddea48ea5bc2
Dylan <mrdlesniak@gmail.com>
2020-06-17 17:27:41 -0700

    set up base.html and added some formatting to the menu

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+def index(request): 
+    context = {
+        "hello": "world",
+    }
+    return render(request, 'portfolio/index.html', context)
