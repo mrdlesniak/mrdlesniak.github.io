e4cbad133cac3aefd8ee5cc96e4159df89038bb3
Ronin <mrdlesniak@gmail.com>
2020-12-05 00:13:37 -0800

    adds slideshow section with working arrows, transitions, and captions. mobile compatibility will be added in next update.

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from .helpers import captions
+import os
+
 
 # Create your views here.
-def index(request): 
+def index(request):
+    picture_files = []
+    for filename in os.listdir('portfolio/static/portfolio/pictures'):
+        url = 'portfolio/pictures/' + filename
+        
+        try:
+            caption = captions.captions_dict[filename]
+        except KeyError:
+            captions.captions_dict[filename] = None
+            caption = captions.captions_dict[filename]
+        
+
+        picture_files.append([url, caption])
+    
+
     context = {
         "activate": "about",
+        "pictures": picture_files,
     }
+    
     return render(request, 'portfolio/index.html', context)
 
 def projects(request):
